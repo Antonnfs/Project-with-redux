9a6e62e01afd8c7f8fa5a6d4ebe403cb4851b74e
Antonnfs <anton.nfs1@gmail.com>
2022-08-19 16:22:25 +0300

    local storage

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/index';
+import { configureStore } from './store/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore()
 root.render(
   <Provider store={ store }>
     <App />
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,37 @@
 import {createStore} from 'redux';
-
 import { rootReducer } from './root-reducer';
+import { loadState, saveState } from './local-storage';
+import { throttle } from 'lodash';
 
-export const store = createStore(rootReducer)
+
+
+// export const store = () => {
+// 	const persistedState = loadState()
+// 	const confStore = createStore(
+// 		rootReducer,
+// 		persistedState,
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	)
+// 	confStore.subscribe(() => {
+// 		saveState(confStore.getState())
+// 	})
+
+// 	return confStore;
+// }
+export const configureStore = () => {
+	const persistedState = loadState()
+	const store = createStore(
+		rootReducer,
+		persistedState,
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+	store.subscribe(throttle(() => {
+		saveState(
+			{
+				todos: store.getState().todos,
+			}
+		)
+	}, 1000))
+
+	return store;
+}
