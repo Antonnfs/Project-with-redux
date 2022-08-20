7603552f4249792f42f9ba65a468f12e3af926f4
Antonnfs <anton.nfs1@gmail.com>
2022-08-20 18:56:28 +0300

    Fixed routing

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
-import { BrowserRouter, Rutes, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Rutes, Route, Routes } from 'react-router-dom';
 
 
 export default function Root({store}) {
 	return (
 		<Provider store={ store }>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<App />}>
 						<Route path=":filter" element={<App/>}/>
 					</Route>
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	)
 }
